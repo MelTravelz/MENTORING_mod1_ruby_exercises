5854241682cc50dd85cb252ec494aa2bdbaf4b7e
Melony Franchini <meltravelz@gmail.com>
2023-07-24 16:07:17 -0600

    test: #initialize

---
+++ practice_independent_challenges/week3/world_cup/spec/player_spec.rb
@@ -0,0 +1,13 @@
+require "./lib/player"
+
+RSpec.describe Player do
+  let(:player) { Player.new({name: "Luka Modric", position: "midfielder"}) }
+  
+  describe "#initialize" do
+    it "exsists & has attributes" do
+      expect(player).to be_a(Player)
+      expect(player.name).to eq("Luka Modric")
+      expect(player.position).to eq("midfielder")
+    end
+  end
+end
