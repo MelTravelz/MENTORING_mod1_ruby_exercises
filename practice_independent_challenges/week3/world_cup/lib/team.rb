6f5d9b274194c8b2076b024d46db7c5a5e5a0358
Melony Franchini <meltravelz@gmail.com>
2023-07-24 16:15:50 -0600

    test & feat: #eliminated?

--- practice_independent_challenges/week3/world_cup/lib/team.rb
+++ practice_independent_challenges/week3/world_cup/lib/team.rb
@@ -1,7 +1,12 @@
 class Team
-  attr_reader :country
-  
+  attr_reader :country, :eliminated
+
   def initialize(country)
     @country = country
+    @eliminated = false
+  end
+
+  def eliminated?
+    @eliminated
   end
 end
--- practice_independent_challenges/week3/world_cup/spec/team_spec.rb
+++ practice_independent_challenges/week3/world_cup/spec/team_spec.rb
@@ -3,10 +3,16 @@ require "./lib/team"
 
 RSpec.describe Team do
   let(:team) { Team.new("France") }
-  describe "#initialize" do
-    it "exsists & had attributes" do
-      expect(team).to be_a(Team)
-      expect(team.country).to eq("France")
-    end
+
+  it "exsists & had attributes" do
+    expect(team).to be_a(Team)
+    expect(team.country).to eq("France")
+    expect(team.eliminated).to eq(false)
   end
+
+  it "checks if team has been eliminated" do
+    expect(team.eliminated?).to eq(false)
+  end
+
+
 end
