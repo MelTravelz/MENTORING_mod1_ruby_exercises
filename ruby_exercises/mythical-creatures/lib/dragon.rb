1404e71987aac7eb2dec55ec91e2186053ee9ee6
Melony Franchini <meltravelz@gmail.com>
2023-08-02 12:50:10 +0300

    Add #eat to Dragon

--- ruby_exercises/mythical-creatures/lib/dragon.rb
+++ ruby_exercises/mythical-creatures/lib/dragon.rb
@@ -8,9 +8,15 @@ class Dragon
     @color = color
     @rider = rider
     @hungry = true
+    @hungry_counter = 0
   end
 
   def hungry?
     @hungry
+  end
+
+  def eat
+    @hungry_counter += 1
+    @hungry = false if @hungry_counter >= 3
   end
 end
