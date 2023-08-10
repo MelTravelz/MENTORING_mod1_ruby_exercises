666a0fffa656852b85f676e02c2c602455441a3b
Melony Franchini <meltravelz@gmail.com>
2023-08-10 08:52:20 +0300

    Add discussion notes on @ & attr_reader

--- ruby_exercises/mythical-creatures/lib/hobbit.rb
+++ ruby_exercises/mythical-creatures/lib/hobbit.rb
@@ -2,16 +2,22 @@ class Hobbit
   attr_reader :name,
               :disposition,
               :age
+              # attributes can be read only & from outside this class/init-method
 
   def initialize(name, disposition = 'homebody')
     @name = name
     @disposition = disposition
     @age = 0
     @short = true
+    #to manipulate data within the class, must call on these attributes directly (with the @ symbol) 
   end
 
   def celebrate_birthday
+    # will work because it's being called directly (can be changed):
     @age += 1
+
+    # wont work because it's being called from the attr_reader (read only):
+    # age += 1
   end
 
   def adult?
@@ -22,22 +28,34 @@ class Hobbit
     # end
 
     #This is a ternary operator:
-    @age >= 33 ? true : false
+    age >= 33 ? true : false
   end
 
   def old?
-    @age >= 101 ? true : false
+    # if @age >= 101
+    #   true
+    # else 
+    #   false
+    # end
+
+    age >= 101 ? true : false
   end
 
   def has_ring?
-    if name == "Frodo"
-      true
-    else
-      false
-    end
+    # if name == "Frodo"
+    #   true
+    # else
+    #   false
+    # end
+
+    name == "Frodo" ? true : false
   end
 
   def short?
+    # must call this with the @ because it was NOT put in the attr_reader (possible to manipulate data here too):
     @short
+
+    # this would also work if commented out @short in initialize:
+    # true
   end
 end
