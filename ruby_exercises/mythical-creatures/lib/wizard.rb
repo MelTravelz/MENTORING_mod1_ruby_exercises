b398ca95fe25bf008e5543a0567ccc466fa81001
Melony Franchini <meltravelz@gmail.com>
2023-10-09 20:18:46 -0600

    Add #bearded?

--- ruby_exercises/mythical-creatures/lib/wizard.rb
+++ ruby_exercises/mythical-creatures/lib/wizard.rb
@@ -3,5 +3,12 @@ class Wizard
 
   def initialize(name)
     @name = name
+    @bearded = true
+  end
+
+  #this type of method calls on a "default" attribute in initialize
+  #we call this method outside the class instead of calling on the attribute directly
+  def bearded?
+    @bearded
   end
 end
